e78c94d3c17f67c9fb5ceee0148b6c542776dfd5
Clément Dreptin <cdreptin@gmail.com>
2026-08-16 19:49:24 +0200

    build: added an imagexex config and a script to increment the version number

---
+++ scripts/version.ps1
@@ -0,0 +1,51 @@
+param (
+    [Parameter(Mandatory = $true, Position = 0)]
+    [ValidateSet("major", "minor", "patch")]
+    [string]$ComponentToIncrement
+)
+
+$ErrorActionPreference = "Stop"
+
+$RootDir = "$PSScriptRoot\.."
+$XmlFilePath = "$RootDir\xex.xml"
+
+Write-Output "Parsing ImageXEX configuration..."
+$Xml = [xml](Get-Content $XmlFilePath)
+
+$Major = [int]$Xml.xex.version.major
+$Minor = [int]$Xml.xex.version.minor
+$Patch = [int]$Xml.xex.version.build
+
+Write-Output "Current version: v$Major.$Minor.$Patch"
+
+switch ($ComponentToIncrement) {
+    "major" {
+        $Major += 1
+        $Minor = 0
+        $Patch = 0
+    }
+    "minor" {
+        $Minor += 1
+        $Patch = 0
+    }
+    "patch" {
+        $Patch += 1
+    }
+}
+
+$Xml.xex.version.major = [string]$Major
+$Xml.xex.version.minor = [string]$Minor
+$Xml.xex.version.build = [string]$Patch
+
+$NewVersionString = "v$Major.$Minor.$Patch"
+Write-Output "New version: $NewVersionString"
+
+Write-Output "Updating ImageXEX configuration with the new version number..."
+$Xml.Save($XmlFilePath)
+
+Write-Output "Committing changes made to ImageXEX configuration..."
+git add "$XmlFilePath"
+git commit -m "$NewVersionString"
+
+Write-Output "Creating tag..."
+git tag -m "$NewVersionString" "$NewVersionString"
